f913e3fafe91c3a356c16983a1704a8cbdea91d9
Bern <bernnystrom@gmail.com>
2022-06-04 18:05:28 +0800

    🎉 (new): Initial commit with base layout and functionality
    Initial commit of project with a basic starting app layout and functionality options for user to select; their desired wake up time, amount of hours of sleep, and daily coffee intake.

---
+++ SleepBetter/SleepBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepBetterApp.swift
+//  SleepBetter
+//
+//  Created by Bern N on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
