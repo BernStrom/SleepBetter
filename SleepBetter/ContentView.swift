53b62dfcc937c2d91c1e18f0f256d6bb2943762a
Bern <bernnystrom@gmail.com>
2022-06-05 01:16:54 +0800

    ✨ (new): Add a mlmodel file for the functionality to calculate user sleep time
    Added a SleepCalculator.mlmodel file for the usage of CoreML in the app to make predictions on the actual sleep time the user needs based on their input values on the app interface.

--- SleepBetter/ContentView.swift
+++ SleepBetter/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Bern N on 6/3/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -32,6 +33,24 @@ struct ContentView: View {
                 Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
             }
             .navigationTitle("SleepBetter")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
+    }
+    
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+        } catch {
+            // code catch errors here in case the above do block fails.
         }
     }
 }
