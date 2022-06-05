b6d6179cf0b1e1947bfb5b2005e4c52de89973af
Bern <bernnystrom@gmail.com>
2022-06-05 18:20:17 +0800

    🚸 (fix): Change the default wake up time to 7:00 AM
    Added a new static property defaultWakeTime that sets the default time to 7:00 AM and replaced the @State property wakeUp value from Date.now to use the defaultWakeTime value instead, since 7:00 AM represents a more common universal wake up time.

--- SleepBetter/ContentView.swift
+++ SleepBetter/ContentView.swift
@@ -9,12 +9,19 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
         NavigationView {
